8395e3f9e8be12efcc3383ce28f3201968ea2485
delatorre96 <idelatorrecubillo@gmail.com>
2025-05-05 16:42:34 +0200

    Re Normalizacion

---
+++ normalizacionTMM.r
@@ -0,0 +1,15 @@
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+BiocManager::install("edgeR")
+
+library(edgeR)
+
+counts <- read.csv("C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/df_raw_A_Normalizar.csv")
+dge <- DGEList(counts = counts)
+dge <- calcNormFactors(dge, method = "TMM")
+dge$samples$norm.factors
+
+eff.lib.size <- dge$samples$lib.size * dge$samples$norm.factors
+
+cpm_tmm <- cpm(dge, normalized.lib.sizes = TRUE)
+
+write.csv(cpm_tmm, file = "C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/cpm_tmm.csv")

23a7f11f408098880a35e4cf02115a8474ea0657
delatorre96 <idelatorrecubillo@gmail.com>
2025-06-02 13:43:58 +0200

    RNA selected

--- normalizacionTMM.r
+++ normalizacionTMM.r
@@ -3,7 +3,7 @@ BiocManager::install("edgeR")
 
 library(edgeR)
 
-counts <- read.csv("C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/df_raw_A_Normalizar.csv")
+counts <- read.csv("C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/df_raw_Proteines_Normalizar.csv")
 dge <- DGEList(counts = counts)
 dge <- calcNormFactors(dge, method = "TMM")
 dge$samples$norm.factors
@@ -12,4 +12,4 @@ eff.lib.size <- dge$samples$lib.size * dge$samples$norm.factors
 
 cpm_tmm <- cpm(dge, normalized.lib.sizes = TRUE)
 
-write.csv(cpm_tmm, file = "C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/cpm_tmm.csv")
+write.csv(cpm_tmm, file = "C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/cpm_tmmProteines.csv")

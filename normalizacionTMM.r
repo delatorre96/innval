7647650f6d11520ef0ac9d869371853fd6670333
delatorre96 <idelatorrecubillo@gmail.com>
2025-07-11 13:45:50 +0200

    relacionMrnaLncrna_normalizacion_validacion

--- normalizacionTMM.r
+++ normalizacionTMM.r
@@ -3,13 +3,18 @@ BiocManager::install("edgeR")
 
 library(edgeR)
 
-counts <- read.csv("C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/df_raw_Proteines_Normalizar.csv")
-dge <- DGEList(counts = counts)
-dge <- calcNormFactors(dge, method = "TMM")
-dge$samples$norm.factors
+normalize <- function(csv_input_path,csv_output_path) { 
+  counts <- read.csv(csv_input_path)
+  dge <- DGEList(counts = counts)
+    dge <- calcNormFactors(dge, method = "TMM")
+    dge$samples$norm.factors
 
-eff.lib.size <- dge$samples$lib.size * dge$samples$norm.factors
+    eff.lib.size <- dge$samples$lib.size * dge$samples$norm.factors
 
-cpm_tmm <- cpm(dge, normalized.lib.sizes = TRUE)
+    cpm_tmm <- cpm(dge, normalized.lib.sizes = TRUE)
+    write.csv(cpm_tmm, file = csv_output_path)
+}
 
-write.csv(cpm_tmm, file = "C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/cpm_tmmProteines.csv")
+ #"C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/cpm_tmmProteines.csv"
+normalize('C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/lncRNA_validacion.csv','C:/Users/delatorrei/Desktop/Proyectos/INNVAL/innval/lncRNA_validacion_norm.csv')
+
